5909ab6993c0de580eb5d539bfa7c395057e1347
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-02-24 18:52:05 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/sprint-1-Testing' into sprint-1""
    This reverts commit 666b9be09eff5abf3f9c0324484170863944ae58.

---
+++ openTill/openTill.Testing/[ClassBeingTested]Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace openTill.Testing {
+    [TestClass]
+    public class ClassBeingTestedTest {
+        [TestMethod]
+        public void BeDescriptiveAsIn_CheckingThatTrueEqualsTrue_BeingLongHereDoesNotMatter() {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void HowToDoTestsThatShouldFail_InThisCaseIndexOutOfBounds() {
+            int[] testIntArray = new int[2];
+            int testInt = testIntArray[2];
+        }
+
+        [TestMethod]
+        public void CheckTwoArraysAreEqual() {
+            int[] firstTestArray = new int[2] { 0, 0 };
+            int[] secondTestArray = new int[2] { 0, 0 };
+            CollectionAssert.AreEqual(firstTestArray, secondTestArray);
+        }
+    }
+}
