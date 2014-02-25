5cc878dcd12b6b89350123055008927bf46c952c
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-02-24 20:47:03 -0500

    Added IProductService

---
+++ openTill/openTill.GUI/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using openTill.Domain;
+using System;
+using System.Collections.Generic;
+namespace openTill.GUI
+{
+    interface IProductService
+    {
+        List<openTill.Domain.Product> GetAllProducts();
+        Product GetProductByUPC(string givenUPC);
+        List<openTill.Domain.Product> GetProductsByBrand(string givenBrand);
+    }
+}
