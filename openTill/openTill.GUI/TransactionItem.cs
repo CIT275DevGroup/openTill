bf8758eec35b6cb33e6a5db3e504e94770858c30
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-02-24 20:47:16 -0500

    Added TransactionItem

---
+++ openTill/openTill.GUI/TransactionItem.cs
@@ -0,0 +1,39 @@
+﻿using openTill.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openTill.GUI
+{
+    class TransactionItem : ObservableObject
+    {
+        private Product _item;
+
+        public Product Item
+        {
+            get { return _item; }
+            set 
+            { 
+                _item = value;
+                RaisePropertyChanged("Item");
+            }
+        }
+        private int _quantity;
+       
+        public int Quantity
+        {
+            get { return _quantity; }
+            set 
+            { 
+                _quantity = value;
+                RaisePropertyChanged("Quantity");
+            }
+        }
+        public TransactionItem(Product item)
+        {
+            _item = item;
+        }
+    }
+}
