ce5f6fcd0ce33db6c07a53e031826fa5b31bd002
akhier <akhierthedragonhearted@yahoo.com>
2014-03-02 23:33:10 -0500

    +Persistence.BrandRepositoryTest
    Added the exception tests for it, more still needed

---
+++ openTill/openTill.Testing/Persistence.BrandRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using openTill.Persistence;
+
+namespace openTill.Testing {
+    [TestClass]
+    public class BrandRepositoryTest {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullDTOInsertException() {
+            BrandRepository testBrandRepository = new BrandRepository();
+            testBrandRepository.Insert(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullDTOUpdateException() {
+            BrandRepository testBrandRepository = new BrandRepository();
+            testBrandRepository.Update(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullDTODeleteException() {
+            BrandRepository testBrandRepository = new BrandRepository();
+            testBrandRepository.Delete(null);
+        }
+    }
+}
