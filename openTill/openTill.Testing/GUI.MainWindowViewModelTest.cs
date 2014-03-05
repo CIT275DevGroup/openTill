b3b20d4fb201fd40c04a9de291e9eb6d093f597a
Cameron VanHouzen <camvh@live.com>
2014-03-05 12:44:31 -0500

    Added MainWindow Tests and Moq framework

---
+++ openTill/openTill.Testing/GUI.MainWindowViewModelTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using openTill.GUI;
+
+namespace openTill.Testing
+{
+    [TestClass]
+    public class MainWindowViewModelTest
+    {
+        [TestMethod]
+        public void InstantiateTest()
+        {
+            MainWindowViewModel testVM = new MainWindowViewModel();
+            Assert.IsNotNull(testVM);
+        }
+        [TestMethod]
+        public void GetProductTest()
+        {
+
+        }
+    }
+}
