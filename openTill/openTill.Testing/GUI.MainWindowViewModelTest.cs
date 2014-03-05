2611853dfe534eba1c99b6744c0d2d53d305b2b2
Cameron VanHouzen <camvh@live.com>
2014-03-05 14:29:49 -0500

    Added Testing Suite for MWVM

--- openTill/openTill.Testing/GUI.MainWindowViewModelTest.cs
+++ openTill/openTill.Testing/GUI.MainWindowViewModelTest.cs
@@ -44,5 +44,82 @@ namespace openTill.Testing
             testVM.AddTransactionItem("1");
             CollectionAssert.AreEquivalent(mockProduct, testVM.TransactionProducts.Select(x => x.Item).ToList());
         }
+        [TestMethod]
+        public void ChangeQty()
+        {
+            var mock = new Mock<IProductService>();
+            List<ProductDTO> mockProduct = new List<ProductDTO>{
+                    new ProductDTO
+                    {
+                        UPC = "1"
+                    }};
+            mock.Setup(framework => framework.GetAllProducts())
+                .Returns(mockProduct);
+            mock.Setup(framework => framework.GetProductByUPC("1"))
+                .Returns(mockProduct.First(x => x.UPC == "1"));
+            MainWindowViewModel testVM = new MainWindowViewModel(mock.Object);
+            testVM.AddTransactionItem("1");
+            testVM.SelectedItem = testVM.TransactionProducts[0];
+            testVM.ChangeItemQuantity(3);
+            Assert.AreEqual(4, testVM.TransactionProducts[0].Quantity);
+        }
+        [TestMethod]
+        public void DecreaseQty()
+        {
+            var mock = new Mock<IProductService>();
+            List<ProductDTO> mockProduct = new List<ProductDTO>{
+                    new ProductDTO
+                    {
+                        UPC = "1"
+                    }};
+            mock.Setup(framework => framework.GetAllProducts())
+                .Returns(mockProduct);
+            mock.Setup(framework => framework.GetProductByUPC("1"))
+                .Returns(mockProduct.First(x => x.UPC == "1"));
+            MainWindowViewModel testVM = new MainWindowViewModel(mock.Object);
+            testVM.AddTransactionItem("1");
+            testVM.SelectedItem = testVM.TransactionProducts[0];
+            testVM.ChangeItemQuantity(3);
+            testVM.RemoveTransactionItem();
+            Assert.AreEqual(3, testVM.TransactionProducts[0].Quantity);
+        }
+        [TestMethod]
+        public void AddDuplicateTest()
+        {
+            var mock = new Mock<IProductService>();
+            List<ProductDTO> mockProduct = new List<ProductDTO>{
+                    new ProductDTO
+                    {
+                        UPC = "1"
+                    }};
+            mock.Setup(framework => framework.GetAllProducts())
+                .Returns(mockProduct);
+            mock.Setup(framework => framework.GetProductByUPC("1"))
+                .Returns(mockProduct.First(x => x.UPC == "1"));
+            MainWindowViewModel testVM = new MainWindowViewModel(mock.Object);
+            testVM.AddTransactionItem("1");
+            testVM.AddTransactionItem("1");
+            CollectionAssert.AreEquivalent(mockProduct, testVM.TransactionProducts.Select(x => x.Item).ToList());
+            Assert.AreEqual(2, testVM.TransactionProducts[0].Quantity);
+        }
+        [TestMethod]
+        public void RemoveProductTest()
+        {
+            var mock = new Mock<IProductService>();
+            List<ProductDTO> mockProduct = new List<ProductDTO>{
+                    new ProductDTO
+                    {
+                        UPC = "1"
+                    }};
+            mock.Setup(framework => framework.GetAllProducts())
+                .Returns(mockProduct);
+            mock.Setup(framework => framework.GetProductByUPC("1"))
+                .Returns(mockProduct.First(x => x.UPC == "1"));
+            MainWindowViewModel testVM = new MainWindowViewModel(mock.Object);
+            testVM.AddTransactionItem("1");
+            testVM.SelectedItem = testVM.TransactionProducts[0];
+            testVM.RemoveTransactionItem();
+            Assert.IsTrue(testVM.TransactionProducts.Count == 0);
+        }
     }
 }
