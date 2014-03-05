da05a3a83ab52e4721d3db8f887526931ae51ad4
Cameron VanHouzen <camvh@live.com>
2014-03-05 14:08:33 -0500

    Added Tests for MWVM

--- openTill/openTill.Testing/GUI.MainWindowViewModelTest.cs
+++ openTill/openTill.Testing/GUI.MainWindowViewModelTest.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using openTill.GUI;
+using Moq;
+using openTill.Domain;
+using openTill.Domain.Services;
+using openTill.Domain.DTO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace openTill.Testing
 {
@@ -10,13 +16,33 @@ namespace openTill.Testing
         [TestMethod]
         public void InstantiateTest()
         {
-            MainWindowViewModel testVM = new MainWindowViewModel();
+            var mock = new Mock<IProductService>();
+            List<ProductDTO> mockProduct = new List<ProductDTO>{
+                    new ProductDTO
+                    {
+                        UPC = "1"
+                    }};
+            mock.Setup(framework => framework.GetAllProducts())
+                .Returns(mockProduct);
+            MainWindowViewModel testVM = new MainWindowViewModel(mock.Object);
             Assert.IsNotNull(testVM);
         }
         [TestMethod]
         public void GetProductTest()
         {
-
+            var mock = new Mock<IProductService>();
+            List<ProductDTO> mockProduct = new List<ProductDTO>{
+                    new ProductDTO
+                    {
+                        UPC = "1"
+                    }};
+            mock.Setup(framework => framework.GetAllProducts())
+                .Returns(mockProduct);
+            mock.Setup(framework => framework.GetProductByUPC("1"))
+                .Returns(mockProduct.First(x => x.UPC == "1"));
+            MainWindowViewModel testVM = new MainWindowViewModel(mock.Object);
+            testVM.AddTransactionItem("1");
+            CollectionAssert.AreEquivalent(mockProduct, testVM.TransactionProducts.Select(x => x.Item).ToList());
         }
     }
 }
