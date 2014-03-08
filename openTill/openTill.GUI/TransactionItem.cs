79f3ddaae20ed5b81002027b8d224e5e3869a5c3
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-08 11:58:55 -0500

    Documented TransactionItem

--- openTill/openTill.GUI/TransactionItem.cs
+++ openTill/openTill.GUI/TransactionItem.cs
@@ -8,10 +8,15 @@ using System.Threading.Tasks;
 
 namespace openTill.GUI
 {
+    /// <summary>
+    /// Wrapper model for ProductDTO, adds Quantity property
+    /// </summary>
     public class TransactionItem : ObservableObject
     {
         private ProductDTO _item;
-
+        /// <summary>
+        /// The ProductDTO associated with this TransactionItem
+        /// </summary>
         public ProductDTO Item
         {
             get { return _item; }
@@ -22,7 +27,9 @@ namespace openTill.GUI
             }
         }
         private int _quantity = 1;
-       
+       /// <summary>
+       /// The quantity of the ProductDTO represented by this object
+       /// </summary>
         public int Quantity
         {
             get { return _quantity; }
@@ -32,6 +39,10 @@ namespace openTill.GUI
                 RaisePropertyChanged("Quantity");
             }
         }
+        /// <summary>
+        /// Default constructor, sets the ProductDTO represented by this TransactionItem
+        /// </summary>
+        /// <param name="item">the ProductDTO this object will represent</param>
         public TransactionItem(ProductDTO item)
         {
             _item = item;
