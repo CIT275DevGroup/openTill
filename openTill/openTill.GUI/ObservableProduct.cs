b87092a002acec536b07ab48d3a0aa840d215a41
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-08 16:06:38 -0500

    Added ObservableProduct

---
+++ openTill/openTill.GUI/ObservableProduct.cs
@@ -0,0 +1,180 @@
+﻿using openTill.Domain;
+using openTill.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openTill.GUI
+{
+    public class ObservableProduct : ObservableObject
+    {
+        private string _upc;
+        private int _categoryListID;
+        private int _brandID;
+        private string _name;
+        private string _description;
+        private decimal _storeCost;
+        private decimal _sellingPrice;
+        private int _minOnHand;
+        private int _onHand;
+        private bool _hasDeposit;
+        private bool _isTaxable;
+        private int _minimumAge;
+        private Brand _brand;
+        private List<ProductCategory> _categoryList;
+        private List<SaleItem> _saleItem;
+
+        public string UPC
+        {
+            get { return _upc; }
+            set 
+            {
+                _upc = value;
+                RaisePropertyChanged("UPC");
+            }
+        }
+
+        public int CategoryListID
+        {
+            get { return _categoryListID; }
+            set 
+            {
+                _categoryListID = value;
+                RaisePropertyChanged("CategoryListID");
+            }
+        }
+
+        public int BrandID
+        {
+            get { return _brandID; }
+            set
+            {
+                _brandID = value;
+                RaisePropertyChanged("BrandID");
+            }
+
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                _name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set 
+            {
+                _description = value;
+                RaisePropertyChanged("Description");
+            }
+        }
+
+        public decimal StoreCost
+        {
+            get { return _storeCost; }
+            set 
+            {
+                _storeCost = value;
+                RaisePropertyChanged("StoreCost");
+            }
+        }
+
+        public decimal SellingPrice
+        {
+            get { return _sellingPrice; }
+            set 
+            {
+                _sellingPrice = value;
+                RaisePropertyChanged("SellingPrice");
+            }
+        }
+
+        public int OnHand
+        {
+            get { return _onHand; }
+            set 
+            {
+                _onHand = value;
+                RaisePropertyChanged("OnHand");
+            }
+        }
+
+        public int MinOnHand
+        {
+            get { return _minOnHand; }
+            set 
+            {
+                _minOnHand = value;
+                RaisePropertyChanged("MinOnHand");
+            }
+        }
+
+        public bool HasDeposit
+        {
+            get { return _hasDeposit; }
+            set 
+            {
+                _hasDeposit = value;
+                RaisePropertyChanged("HasDeposit");
+            }
+        }
+
+        public bool IsTaxable
+        {
+            get { return _isTaxable; }
+            set 
+            {
+                _isTaxable = value;
+                RaisePropertyChanged("IsTaxable");
+            }
+        }
+
+        public int MinimumAge
+        {
+            get { return _minimumAge; }
+            set 
+            {
+                _minimumAge = value;
+                RaisePropertyChanged("MinimumAge");
+            }
+        }
+
+        public Brand Brand
+        {
+            get { return _brand; }
+            set
+            {
+                _brand = value;
+                RaisePropertyChanged("Brand");
+            }
+        }
+
+        public List<ProductCategory> CategoryList
+        {
+            get { return _categoryList; }
+            set 
+            {
+                _categoryList = value;
+                RaisePropertyChanged("CategoryList");
+            }
+        }
+
+        public List<SaleItem> SaleItem
+        {
+            get { return _saleItem; }
+            set 
+            {
+                _saleItem = value;
+                RaisePropertyChanged("SaleItem");
+            }
+        }
+    }
+}
