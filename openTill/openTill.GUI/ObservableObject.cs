5f51daf51ba2ef12264e25daa34d55291c8b38b2
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-08 11:58:43 -0500

    Documented ObservableObject

--- openTill/openTill.GUI/ObservableObject.cs
+++ openTill/openTill.GUI/ObservableObject.cs
@@ -7,8 +7,14 @@ using System.Threading.Tasks;
 
 namespace openTill.GUI
 {
+    /// <summary>
+    /// Base class to wrap the INotifyPropertyChanged interface
+    /// </summary>
     public class ObservableObject : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Event to be raised on a property changing
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void RaisePropertyChanged(string propertyName)
