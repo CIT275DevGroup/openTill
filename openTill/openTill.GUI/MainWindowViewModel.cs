5df9a9f98e9aafcf74e099b83c6584dbe5e91a1a
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-08 11:54:10 -0500

    Added XML documentation to MWVM

--- openTill/openTill.GUI/MainWindowViewModel.cs
+++ openTill/openTill.GUI/MainWindowViewModel.cs
@@ -12,19 +12,31 @@ using openTill.Domain.Interface.Service;
 
 namespace openTill.GUI
 {
+    /// <summary>
+    /// Contains state and commands for the Main Window
+    /// </summary>
     public class MainWindowViewModel : ObservableObject
     {
+        /// <summary>
+        /// Default constructor, instantiates a ProductService
+        /// </summary>
         public MainWindowViewModel()
         {
             this.productService = new ProductService(new ProductRepository());
         }
+        /// <summary>
+        /// Constructor taking an IProductService for testing
+        /// </summary>
+        /// <param name="productService"></param>
         public MainWindowViewModel(IProductService productService)
         {
             this.productService = productService;
         }
         private IProductService productService;
         private ObservableCollection<TransactionItem> _transactionProducts = new ObservableCollection<TransactionItem>();
-
+        /// <summary>
+        /// ObservableCollection of ProductDTO wrapped with quantity and price properties
+        /// </summary>
         public ObservableCollection<TransactionItem> TransactionProducts
         {
             get { return _transactionProducts; }
@@ -34,7 +46,9 @@ namespace openTill.GUI
             }
         }
         private TransactionItem _selectedItem;
-
+        /// <summary>
+        /// Represents currently selected item from TransactionProducts
+        /// </summary>
         public TransactionItem SelectedItem
         {
             get { return _selectedItem; }
@@ -44,7 +58,10 @@ namespace openTill.GUI
                 RaisePropertyChanged("SelectedItem");
             }
         }
-
+        /// <summary>
+        /// Uses IProductService to try and pull the product with the associated UPC, adds the product to Transaction Items if successful
+        /// </summary>
+        /// <param name="UPC">The UPC code of the product to add</param>
         public void AddTransactionItem(string UPC)
         {
             if (TransactionProducts.Any(x => x.Item.UPC == UPC))
@@ -61,6 +78,9 @@ namespace openTill.GUI
                     throw new ArgumentException("UPC: " + UPC + "Not found in the system");
             }
         }
+        /// <summary>
+        /// Will remove the SelectedItem from TransactionItems, if not null
+        /// </summary>
         public void RemoveTransactionItem()
         {
             if (SelectedItem != null)
@@ -73,6 +93,10 @@ namespace openTill.GUI
                 }
             }
         }
+        /// <summary>
+        /// Changes the quantity property of the SelectedItem
+        /// </summary>
+        /// <param name="amount">amount by which to modify the quantity of the SelectedItem</param>
         public void ChangeItemQuantity(int amount)
         {
             SelectedItem.Quantity += amount;
