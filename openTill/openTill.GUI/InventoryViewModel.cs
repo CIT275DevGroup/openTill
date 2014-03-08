23fd3e36c88275c8ff496e4d189001cf4d9e7908
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-03-08 16:06:48 -0500

    Bound ManageInventory

--- openTill/openTill.GUI/InventoryViewModel.cs
+++ openTill/openTill.GUI/InventoryViewModel.cs
@@ -1,18 +1,50 @@
-﻿using openTill.Domain.Interface.Service;
+﻿using openTill.Domain.DTO;
+using openTill.Domain.Interface.Repository;
+using openTill.Domain.Interface.Service;
+using openTill.Domain.Services;
+using openTill.Persistence;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace openTill.GUI
 {
-    class InventoryViewModel
+    public class InventoryViewModel : ObservableObject
     {
         private IProductService productService;
+        private BrandDTO[] _brands;
+
+        public BrandDTO[] Brands
+        {
+            get { return _brands; }
+            set { _brands = value; }
+        }
+        
+        private ObservableCollection<ObservableProduct> _products;
+
+        public ObservableCollection<ObservableProduct> Products
+        {
+            get { return _products; }
+            set { _products = value; }
+        }
+        private ObservableProduct _selectedProduct;
+
+        public ObservableProduct SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            {
+                _selectedProduct = value;
+                RaisePropertyChanged("SelectedProduct");
+            }
+        }
         public InventoryViewModel()
         {
-
+            this.productService = new ProductService(new ProductRepository());
+            Brands = new BrandService(new BrandRepository()).GetAllBrands().ToArray();
         }
     }
 }
