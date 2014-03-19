59aaee9d2fff47ceb001a2c754383d44cfa9d141
Cameron VanHouzen <camvh@live.com>
2014-03-19 12:07:25 -0400

    Reorganized InventoryViewModel

--- openTill/openTill.GUI/InventoryViewModel.cs
+++ openTill/openTill.GUI/InventoryViewModel.cs
@@ -10,65 +10,12 @@ namespace openTill.GUI
 {
     public class InventoryViewModel : ObservableObject
     {
-        private IProductService productService;
         private BrandDTO[] _brands;
-
-        /// <summary>
-        /// List of brands for use in product dropdown
-        /// </summary>
-        public BrandDTO[] Brands
-        {
-            get { return _brands; }
-            set { _brands = value; }
-        }
-
         private ObservableCollection<ObservableProduct> _products;
-
-        /// <summary>
-        /// Internal collection of products for use by the view
-        /// </summary>
-        public ObservableCollection<ObservableProduct> Products
-        {
-            get { return _products; }
-            set { _products = value; }
-        }
-
         private ObservableProduct _selectedProduct = new ObservableProduct(new ProductDTO());
-
-        /// <summary>
-        /// The currently selected product, may not be part of Products collection
-        /// </summary>
-        public ObservableProduct SelectedProduct
-        {
-            get { return _selectedProduct; }
-            set
-            {
-                _selectedProduct = value;
-                RaisePropertyChanged("SelectedProduct");
-            }
-        }
-
         private AddProductCommand addCommand;
-
-        /// <summary>
-        /// Instance of a command for adding the selected product to Products collection
-        /// </summary>
-        public AddProductCommand AddCommand
-        {
-            get { return addCommand; }
-            private set { addCommand = value; }
-        }
-
+        private IProductService productService;
         private RemoveProductCommand removeCommand;
-
-        /// <summary>
-        /// Instance of a command for removing the selected product from the Products collection
-        /// </summary>
-        public RemoveProductCommand RemoveCommand
-        {
-            get { return removeCommand; }
-            private set { removeCommand = value; }
-        }
 
         /// <summary>
         /// Default constructor, hooks up to the product and brand services from openTill.Domain
@@ -91,6 +38,53 @@ namespace openTill.GUI
         {
             this.productService = productService;
             Brands = brandService.GetAllBrands().ToArray();
+        }
+
+        /// <summary>
+        /// Instance of a command for adding the selected product to Products collection
+        /// </summary>
+        public AddProductCommand AddCommand
+        {
+            get { return addCommand; }
+            private set { addCommand = value; }
+        }
+
+        /// <summary>
+        /// List of brands for use in product dropdown
+        /// </summary>
+        public BrandDTO[] Brands
+        {
+            get { return _brands; }
+            set { _brands = value; }
+        }
+        /// <summary>
+        /// Internal collection of products for use by the view
+        /// </summary>
+        public ObservableCollection<ObservableProduct> Products
+        {
+            get { return _products; }
+            set { _products = value; }
+        }
+        /// <summary>
+        /// Instance of a command for removing the selected product from the Products collection
+        /// </summary>
+        public RemoveProductCommand RemoveCommand
+        {
+            get { return removeCommand; }
+            private set { removeCommand = value; }
+        }
+
+        /// <summary>
+        /// The currently selected product, may not be part of Products collection
+        /// </summary>
+        public ObservableProduct SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                RaisePropertyChanged("SelectedProduct");
+            }
         }
     }
 }
