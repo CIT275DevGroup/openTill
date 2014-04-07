ab27a6058f124fa11a1b962eced3c68bf8631f62
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-04-06 21:35:22 -0400

    added dev inventory view

---
+++ openTill/openTill.GUI/Views/InventoryLite.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace openTill.GUI.Views
+{
+    /// <summary>
+    /// Description for InventoryLite.
+    /// </summary>
+    public partial class InventoryLite : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the InventoryLite class.
+        /// </summary>
+        public InventoryLite()
+        {
+            InitializeComponent();
+        }
+    }
+}
