d45e60eecc986da53720a8aeea82c352a569eba9
starlinste <articulous@gmail.com>
2014-04-09 13:55:14 -0400

    Numeric buttons should now add values to txtEntry in NoScan.xaml

--- openTill/openTill.GUI/NoScan.xaml.cs
+++ openTill/openTill.GUI/NoScan.xaml.cs
@@ -23,5 +23,55 @@ namespace openTill.GUI
 		{
 			InitializeComponent();
 		}
+
+		private void btnZero_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "0";
+		}
+
+		private void btnOne_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "1";
+		}
+
+		private void btnTwo_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "2";
+		}
+
+		private void btnThree_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "3";
+		}
+
+		private void btnFour_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "4";
+		}
+
+		private void btnFive_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "5";
+		}
+
+		private void btnSix_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "6";
+		}
+
+		private void btnSeven_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "7";
+		}
+
+		private void btnEight_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text + "8";
+		}
+
+		private void btnNine_Click(object sender, RoutedEventArgs e)
+		{
+			txtEntry.Text = txtEntry.Text = "9";
+		}
 	}
 }
